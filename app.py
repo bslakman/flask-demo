daeab074b4085749047e12e9c7577567cc057c7a
Belinda Slakman <slakman.b@husky.neu.edu>
2017-03-20 11:35:20 -0400

    Hopefully get the port binding working for heroku.

--- app.py
+++ app.py
@@ -31,4 +31,5 @@ def result():
 ###################################################################################
 
 if __name__ == '__main__':
-  app.run(port=33507)
+    port = int(os.environ.get("PORT", 33507))
+    app.run(port=port)
